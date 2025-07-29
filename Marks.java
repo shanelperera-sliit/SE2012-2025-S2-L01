af407972296d96a2b914efe063472468949c4959
shanelperera-sliit <it24101845@my.sliit.lk>
2025-07-29 12:23:34 +0530

    Update Marks

--- Marks.java
+++ Marks.java
@@ -4,12 +4,23 @@ public class Marks {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("*** MAIN MENU ***");
-		System.out.println("Add student marks: add [studentID]");
-		System.out.println("Update student marks: update [studentID] [subjectID]");
-		System.out.println("Get the average for a subject: average_s [studentID]");
-		System.out.println("Get the average for a student: average [studentID]");
-		System.out.println("Get the total mark of a student: total [studentID]");
+		int n;
+		int marks[][];
+		
+		final int NUMOFSUBJECTS = 3;
+		String[] subjects = {"Mathematics", "Chemistry", "Physics"};
+		
+		System.out.print("Enter number of students: ");
+		n = scanner.nextInt();
+		
+		while(true) {
+			System.out.println("*** MAIN MENU ***");
+			System.out.println("Add student marks: add [studentID]");
+			System.out.println("Update student marks: update [studentID] [subjectID]");
+			System.out.println("Get the average for a subject: average_s [studentID]");
+			System.out.println("Get the average for a student: average [studentID]");
+			System.out.println("Get the total mark of a student: total [studentID]");
+		}
 		
 		System.out.print("Enter Command: ");
 		String command = scanner.nextLine();
